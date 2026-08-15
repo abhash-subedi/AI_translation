da32bedd900d8c576fbf3df1729fe09e25a9ddf3
a314159265 <abhash.subedi@renegadeinsurance.com>
2026-08-15 15:59:10 +0545

    done first implementation of using groq to make translator

--- translator.py
+++ translator.py
@@ -2,20 +2,20 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
 
-# Initialize the Groq client
+# Initialize Groq client
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def translate_text(text_to_translate, target_language="English"):
     """Sends text to Groq for translation."""
     response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
+    model='llama-3.1-8b-instant',
     messages=[
         {
             'role':'system',
-            'content':f'translate the following message given by user into {target_language}'
+            'content':f'translate the following message given by user into {target_language} also show roman english translation'
         },{
             'role':'user',
             'content':text_to_translate
@@ -26,6 +26,7 @@ def translate_text(text_to_translate, target_language="English"):
     
 def main():
     
+    # loop while rund while text for translation is being passed
     while True:
         text_to_translate = input('enter text to translate: ')
         if text_to_translate.lower() in ['exit']:
